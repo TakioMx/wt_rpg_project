62472ed78990ee5e716b589c169f3838afb37a50
Michael Briller <Michael.Briller@stud.fh-rosenheim.de>
2018-06-26 22:33:02 +0200

    1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Map/>
+  </div>
+</template>
+
+<script>
+import Map from './components/Map'
+
+export default {
+  name: 'App',
+  components: {
+    Map: Map
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Player.vue
@@ -0,0 +1,45 @@
+<template>
+  <span></span>
+</template>
+
+<script>
+export default {
+  name: 'player',
+  data () {
+    return {
+      x: {
+        type: Number,
+        required: true
+      },
+      y: {
+        type: Number,
+        required: true
+      },
+      level: {
+        type: Number,
+        required: true
+      },
+      exp: {
+        type: Number,
+        required: true
+      },
+      health: {
+        type: Number,
+        required: true
+      },
+      potions: {
+        type: Number,
+        required: true
+      },
+      fieldCurrent: {
+        type: String,
+        required: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
